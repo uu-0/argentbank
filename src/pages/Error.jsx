d549187746ba11f4813f93281b34910d82afc2bb
uu-0 <louisepop14@gmail.com>
2025-02-05 17:25:53 +0100

    ajout de la page d'erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from 'react'
+
+import styled from 'styled-components'
+import { colors } from '../style/colors'
+
+const Container = styled.div`
+  background-color: ${colors.bgDark};
+  height: 1100px;
+`
+
+const Title = styled.h1`
+  color: white;
+  font-weight: 600;
+  padding-top: 50px;
+  padding-left: 50px;
+`
+
+const Text = styled.p`
+  color: white;
+  font-weight: 400;
+  padding-left: 50px;
+`
 
 export default function Error() {
   return (
-    <h1>error page</h1>
+    <Container>
+      <Title>
+        Error page
+      </Title>
+      <Text>
+        Are you lost ? invalid URL
+      </Text>
+    </Container>
   )
 }
